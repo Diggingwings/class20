cf1530ac051ac1cc9d104a4eec38653cae845e47
Diggingwings <73551312+Diggingwings@users.noreply.github.com>
2020-11-13 12:00:07 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
-var fixedrec,movingrec
+var fixedrec,movingrec;
 function setup() {
   createCanvas(800,400);
-  fixedrec=createSprite(200,200,50,50)
-fixedrec.shapeColor='blue'
-movingrec=createSprite(400,200,50,50)
-movingrec.shapeColor='blue'
+  fixedrec=createSprite(200,200,50,50);
+fixedrec.shapeColor='blue';
+movingrec=createSprite(400,200,50,50);
+movingrec.shapeColor='blue';
 }
 
 function draw() {
   background(255,255,255);  
-  movingrec.x=World.mouseX
-  movingrec.y=World.mouseY
+  movingrec.x=World.mouseX;
+  movingrec.y=World.mouseY;
   if(movingrec.x-fixedrec.x<movingrec.width/2+fixedrec.width/2&&(fixedrec.x-movingrec.x<fixedrec.width/2+movingrec.width/2)
   &&movingrec.y-fixedrec.y<movingrec.width/2+fixedrec.width/2&&fixedrec.y-movingrec.y<fixedrec.width/2+movingrec.width/2){
     
-    movingrec.shapeColor='green'
+    movingrec.shapeColor='green';
 
-    fixedrec.shapeColor='green'
+    fixedrec.shapeColor='green';
   }
 else{
-  movingrec.shapeColor='blue'
-  fixedrec.shapeColor='blue'
+  movingrec.shapeColor='blue';
+  fixedrec.shapeColor='blue';
 }
   drawSprites();
 }
